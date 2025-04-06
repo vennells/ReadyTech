7b12679747883fcfe5a2ac054cd54156652a2cd0
vennells <andy@gothrive.co>
2025-04-06 23:11:06 +1000

    tidying code

--- ReadyTech.API/Services/BrewCoffeeService.cs
+++ ReadyTech.API/Services/BrewCoffeeService.cs
@@ -3,26 +3,19 @@ using ReadyTech.API.Interfaces;
 
 namespace ReadyTech.API.Services;
 
-public sealed class BrewCoffeeService
+public sealed class BrewCoffeeService(IDateTimeProvider dateTimeProvider)
 {
     private static int _count;
     private static bool isFifthCall => _count % 5 == 0;
-    private readonly IDateTimeProvider _dateTimeProvider;
-
-    public BrewCoffeeService(IDateTimeProvider dateTimeProvider)
-    {
-        _dateTimeProvider = dateTimeProvider;
-    }
+    private readonly IDateTimeProvider _dateTimeProvider = dateTimeProvider;
 
     public BrewCoffee Brew()
     {
-        //if(DateTime.Now.Month == 4 && DateTime.Now.Day == 1)
         if(_dateTimeProvider.Now.Month == 4 && _dateTimeProvider.Now.Day == 1)
         {
             return new BrewCoffee
             {
                 StatusCode = 418,
-                //Message = "I'm a teapot",
             };
         }
 
